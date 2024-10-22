53083c6bceb9915879c58d1966b31e0003cdcda4
Paul K <paulmkouadio@gmail.com>
2024-10-22 12:45:30 +0000

    feat: complete home page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Layout from "./layout";
+import { createContext, useState } from "react";
+
+
+export const NameContext = createContext<null | string>(null)
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [ name, setName ] = useState<string | null>(null)
+
+
+  return (
+    <NameContext.Provider value={name}>
+      <Layout>
+        <Component setName={setName} {...pageProps} />
+      </Layout>
+    </NameContext.Provider>
+  )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,115 +1,93 @@
 import Image from "next/image";
-import localFont from "next/font/local";
+import { worksans } from "@/fonts";
+import { Dispatch, SetStateAction, useContext, useState } from "react";
+import { motion } from "framer-motion"
+import LoopingAnimation from "@/components/Example";
+import Footer from "@/components/Footer";
+import { NameContext } from "./_app";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+export default function Home({setName}: {setName: Dispatch<SetStateAction<string>>}) {
 
-export default function Home() {
+  // const [name, setName] = useState<string>()
+  const topName = useContext(NameContext)
+
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="relative tracking-tight overflow-hidden text-lg">
+      <div className="absolute top-0 h-[120vh] overflow-clip">
+        <div className="bg-gradient-to-b from-transparent w-full h-full absolute top-0 to-black z-20" />
+        <img src="ihor-malytskyi-QZ9PsYOfwuM-unsplash.jpg" alt="" className="z-10 top-0" />
+      </div>
+      <div
+        className="container mx-auto pt-36 relative z-30 h-[120vh]"
+      >
+        <h1 className="text-9xl font-black tracking-tight max-w-2xl leading-[0.8]">The Millennial Christian.</h1>
+        <div className="w-full py-24"></div>
+        <p className="text-lg max-w-[428px] leading-6 tracking-tight">This is a community of Christ-centred whose hearts burn with the desire to see all people come to God and who firmly believe in true community: His body.</p>
+        <div className="pt-11">
+          <motion.span initial={{ x: 400 }} whileInView={{ x: -400 }} className="text-lg">Welcome, </motion.span>
+          <input onChange={(e) => setName(e.target.value)} type="text" placeholder="your name here" className="text-lg border border-white py-1 px-2 w-auto bg-transparent placeholder:text-white placeholder:italic outline-none" />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+      </div>
+      <div>
+        <img src="hannah-busing-gktFjKSWAmA-unsplash.jpg" alt="" className="w-full h-96" />
+      </div>
+      <div className="relative pb-48">
+        <motion.div
+          initial={{ x: `0%` }}
+          animate={{ x: `-27%` }}
+          transition={{ duration: 12, repeat: Infinity, ease: "linear" }}
+          className="grid grid-flow-col absolute gap-[2%]"
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <span className="text-[190px] font-black text-nowrap">{topName} has landed!</span>
+          <span className="text-[190px] font-black text-nowrap">{topName} has landed!</span>
+          <span className="text-[190px] font-black text-nowrap">{topName} has landed!</span>
+          <span className="text-[190px] font-black text-nowrap">{topName} has landed!</span>
+        </motion.div>
+      </div>
+      <div className="container mx-auto grid grid-cols-12 gap-4 py-24">
+        <div className="col-span-5">
+          <p className="text-lg max-w-md">Okay, {topName}! The bread and butter of TMCC is our weekly meetings. We’re not a ministry or a church group, just a community. So when you meet us for the first time, you’re really coming to spend time with family you didn’t know you had!</p>
+          <div className="py-24"></div>
+          <button className="font-black py-2 px-3 uppercase text-lg border border-white">I'm in!</button>
+        </div>
+        <div className="col-span-7">
+          <img src="marek-piwnicki-AsSyn1d2RNs-unsplash.jpg" alt="" className="object-cover w-full" />
+        </div>
+      </div>
+      <div className="container mx-auto grid grid-cols-12 gap-4 h-[80vh] py-24">
+        <div className="col-span-5 flex flex-col justify-between h-[80vh]">
+          <p className="pt-24 pb-32 text-lg">Family is a recurring theme for us. With Jesus as our eldest brother, we who believe all stand together as one, joining hands in worship of our Father. We’re all looking for genuine fellowship; we’ve got that much in common, {topName}!</p>
+          <img src="oc-gonzalez-A-11N8ItHZo-unsplash.jpg" alt="" className="object-cover h-full" />
+        </div>
+        <div className="col-span-7 h-[80vh]">
+          <img src="abhishek-kirloskar-z1V58WZiXpw-unsplash.jpg" alt="" className="object-cover w-full h-full" />
+        </div>
+      </div>
+      <div className="container mx-auto grid grid-cols-12 gap-4">
+        <motion.span
+          className="font-black text-9xl col-span-10 col-start-3 py-48"
+          initial={{opacity: 0.3}}
+          whileInView={{opacity:1}}
+          viewport={{once:true}}
+          transition={{duration: 1, delay: 1}}
+        >"I don't think I belong."</motion.span>
+        <motion.span
+          className="font-black text-9xl col-span-10 py-48"
+          initial={{opacity: 0.3}}
+          whileInView={{opacity:1}}
+          viewport={{once:true}}
+          transition={{duration: 1, delay: 1}}
+        >"It doesn't feel genuine."</motion.span>
+      </div>
+      <div className="container mx-auto grid py-36 grid-cols-12">
+        <p className="col-span-5 text-lg">Sounds familiar? Unfortunately, these phrases are all to common in our churches. Many of us are looking to do life together, beyond the polished, well-oiled events that are our Sunday services.</p>
+      </div>
+      <div className="h-80">
+        <img src="wallace-henry-_OgsP9BvL20-unsplash.jpg" alt="" className="object-cover h-full w-full"/>
+      </div>
+      <div className="container mx-auto grid py-36 grid-cols-12">
+        <p className="col-span-5 text-lg">TMCC exists to fill a gaping hole in the contemporary church. Yes, Sunday service is attractive, Instagram-worthy and “powerful”, but leaves many young people empty.</p>
+      </div>
     </div>
   );
 }
