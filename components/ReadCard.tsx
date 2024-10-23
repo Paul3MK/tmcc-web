f0026956a534b6b8436b6c3ab30f952ffad232ad
Paul K <paulmkouadio@gmail.com>
2024-10-23 10:21:53 +0000

    refactor: componentise

--- components/ReadCard.tsx
+++ components/ReadCard.tsx
@@ -1,14 +1,16 @@
+import Link from "next/link";
+
 export default function ReadCard({imageLink, tag, title, date}: {imageLink: string, tag: string, title: string, date: Date}) {
     return (
-        <div>
+        <Link href="#" className="group">
             <img src={imageLink} alt="" className="h-80 w-full object-cover" />
-            <div className="border border-white px-2 py-1 my-6 inline-block">
-                <span className="uppercase">{tag}</span>
+            <div className="border border-white group-hover:border-transparent group-hover:bg-white px-2 py-1 my-6 inline-block">
+                <span className="uppercase group-hover:text-black">{tag}</span>
             </div>
             <div className="grid gap-3">
                 <h3 className="text-4xl font-black">{title}</h3>
                 <span>{date.toLocaleDateString()}</span>
             </div>
-        </div>
+        </Link>
     )
 }
